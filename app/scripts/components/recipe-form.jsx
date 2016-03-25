f623810948d51f0ef4c7171e0993279dca9709e3
Bruemor <arymer1@gmail.com>
2016-03-25 12:30:25 -0400

    added component and model folders and files for html to jsx conversion

---
+++ app/scripts/components/recipe-form.jsx
@@ -0,0 +1,6 @@
+var $ = require('jquery');
+var _ = require('underscore');
+var Backbone = require('backbone');
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('backbone-react-component');
